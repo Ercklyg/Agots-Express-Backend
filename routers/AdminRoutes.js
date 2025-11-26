c332ce20345cc8658fa032e91ada730756368633
ReynanEstobo <estoboreynan17@gmail.com>
2025-11-27 02:48:15 +0800

    Login Module fixed

--- routers/AdminRoutes.js
+++ routers/AdminRoutes.js
@@ -1,10 +1,12 @@
-import * as AdminController from "../controllers/AdminControllers.js";
 import express from "express";
+import * as AdminController from "../controllers/AdminControllers.js";
 
 const AdminRoutes = express.Router();
 
 AdminRoutes.get("/all", AdminController.Getall);
 AdminRoutes.post("/register", AdminController.register);
 AdminRoutes.post("/login", AdminController.login);
+
+//testing
 
 export default AdminRoutes;
